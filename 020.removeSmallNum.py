49f245545e8f32ca5fe2e6a59c643f660a299b41
hjsunsun <h.js.h0ngjisun@gmail.com>
2021-01-12 14:14:15 +0900

    프로그래머스_제일 작은 수 제거하기

---
+++ 020.removeSmallNum.py
@@ -0,0 +1,23 @@
+def solution(arr):
+    answer = []
+
+    if len(arr)==1:
+        answer.append(-1)
+        
+    else:
+        deleteNum = arr[0]
+        for i in range(0,len(arr)):
+            if deleteNum > arr[i]:
+                deleteNum = arr[i]
+                
+        for i in range(0, arr.count(deleteNum)):
+            del arr[arr.index(deleteNum)]
+
+        answer = arr
+
+    return answer
+
+#5,7,3,2,6
+arguArr = [1,5,7,3,2,6]
+arr = solution(arguArr)
+print(arr)
