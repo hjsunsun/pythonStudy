fe0869dd8f9717e0f50b552eb7e0b7be91034c04
hjsunsun <h.js.h0ngjisun@gmail.com>
2021-01-28 16:48:26 +0900

    프로그래머스_실패율

--- 036.fail.py
+++ 036.fail.py
@@ -1,16 +1,40 @@
 import operator
 def solution(N, stages):
+
     answer = []
 
-    failStages = {}
+    failStages = {} 
+    stage = {} 
+
 
     for i in range(0,N):
-        if stages.count(i+1)==0:
-            failStages[i+1] = 0
+        cnt = 0
+        for j in range(0, len(stages)):
+            if i < stages[j]:
+                cnt += 1
+                failStages[i+1] = cnt
+                
+    for i in range(0,N):
+        stage[i+1] = 0
+        if i==N-1:
+            try:
+                stage[i+1] = failStages[i+1]
+            except:
+                pass
+            break
+        
+        try:
+            stage[i+1] = failStages[i+1]/failStages[i+2]
+        except:
+            pass
 
-        for j in range(0,stages.count(i+1)):
-            failStages[i+1] = j+1
+    for i in list(stage.keys()):
+        for key, value in stage.items():
+            if value == max(stage.values()):
+                answer.append(key)
+                del stage[key]
+                break
 
     return answer
 
-print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(5,[0,6,6,0,0,2,3,0]))
